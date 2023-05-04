c6673440116c3e780d5fb5ae1bd207799ec6e513
Mahmud Nabil <nabilsnigdho@gmail.com>
2023-05-04 20:23:40 +0600

    format code

--- exercise/src/routes/+page.ts
+++ exercise/src/routes/+page.ts
@@ -2,10 +2,10 @@ import type { Exercise } from '$lib/data/schema';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch }) => {
-	const exerciseList = await (await fetch(`/api`)).json() as Exercise[];
-	const bodyPartList = await (await fetch('/api/bodyPartList')).json() as string[];
-	const targetList = await (await fetch('/api/targetList')).json() as string[];
-	const equipmentList = await (await fetch('/api/equipmentList')).json() as string[];
+	const exerciseList = (await (await fetch(`/api`)).json()) as Exercise[];
+	const bodyPartList = (await (await fetch('/api/bodyPartList')).json()) as string[];
+	const targetList = (await (await fetch('/api/targetList')).json()) as string[];
+	const equipmentList = (await (await fetch('/api/equipmentList')).json()) as string[];
 
 	return { exerciseList, bodyPartList, targetList, equipmentList };
 }) satisfies PageLoad;
